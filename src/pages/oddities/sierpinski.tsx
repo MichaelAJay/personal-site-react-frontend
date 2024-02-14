91f67717ecd4fc9f6b562f61b3008678b704c04f
mjay <michael@merged.group>
2024-02-13 23:12:57 -0500

    fixed sierpinski viewer

--- src/pages/oddities/sierpinski.tsx
+++ src/pages/oddities/sierpinski.tsx
@@ -3,8 +3,6 @@ import DOMPurify from 'dompurify';
 import { fetchSierpinskiTriangleSVG } from '../../services/goServerService';
 import { ColorPicker, InputNumber, Typography } from 'antd';
 import './Sierpinski.css';
-import Hero from '../../components/Hero';
-import { InfoCircleTwoTone } from '@ant-design/icons';
 import { colorAliases } from '../..';
 import { Color } from 'antd/es/color-picker';
 import './Oddities.css';
@@ -65,49 +63,45 @@ function SierpinksiTriangle() {
   return (
     <div className='oddities-container'>
       {isBEHealty ? 
-             <Hero>
-             <InfoCircleTwoTone
-               className="sierpinski-info-icon"
-               twoToneColor={colorAliases.lightGreen}
-             />
-             <div
-               className="Sierpinski-triangle-viewer"
-               dangerouslySetInnerHTML={{ __html: svgData }}
-             />
-             <div className="sierpinski-controls-container">
-               <div className="sierpinski-iterations-control">
-                 <label htmlFor={CONTROL_ID.iterationController}>
-                   Number of iterations
-                 </label>
-                 <InputNumber
-                   id={CONTROL_ID.iterationController}
-                   className="Sierpinski-input-number"
-                   min={MIN_DEPTH}
-                   max={MAX_DEPTH}
-                   defaultValue={MIN_DEPTH}
-                   onChange={onIterationChange}
-                 />
-               </div>
-               <div className="sierpinski-color-control">
-                 <label htmlFor={CONTROL_ID.colorController}>Pick a color!</label>
-                 <div id={CONTROL_ID.colorController}>
-                   <ColorPicker
-                     onChangeComplete={onColorChange}
-                     defaultValue={colorAliases.lightGreen}
-                     size={'large'}
-                   />
-                 </div>
-               </div>
-             </div>
-           </Hero>
-           : <BackendUnavailable>
-            <Paragraph>
-              {BE_NOT_HEALTY_MSG}
-              <a href='https://en.wikipedia.org/wiki/Sierpi%C5%84ski_triangle'>on Wikipedia</a>
-            </Paragraph>
-           </BackendUnavailable>
+        <div className='Sierpinski-container'>
+          <div
+            className="Sierpinski-triangle-viewer"
+            dangerouslySetInnerHTML={{ __html: svgData }}
+          />
+          <div className="sierpinski-controls-container">
+            <div className="sierpinski-iterations-control">
+              <label htmlFor={CONTROL_ID.iterationController}>
+                Number of iterations
+              </label>
+              <InputNumber
+                id={CONTROL_ID.iterationController}
+                className="Sierpinski-input-number"
+                min={MIN_DEPTH}
+                max={MAX_DEPTH}
+                defaultValue={MIN_DEPTH}
+                onChange={onIterationChange}
+              />
+            </div>
+            <div className="sierpinski-color-control">
+              <label htmlFor={CONTROL_ID.colorController}>Pick a color!</label>
+              <div id={CONTROL_ID.colorController}>
+                <ColorPicker
+                  onChangeComplete={onColorChange}
+                  defaultValue={colorAliases.lightGreen}
+                  size={'large'}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        : 
+        <BackendUnavailable>
+          <Paragraph>
+            {BE_NOT_HEALTY_MSG}
+            <a href='https://en.wikipedia.org/wiki/Sierpi%C5%84ski_triangle'>on Wikipedia</a>
+          </Paragraph>
+        </BackendUnavailable>
       }
-
     </div>
   );
 }
